d7db16c86c442e98bd429d5d0f14bca68cba68d6
Andrey Bakuta <anb@anahoret.com>
2017-08-31 17:49:52 +0300

    Added jest tests

---
+++ src/sagas/__test__/comments.test.js
@@ -0,0 +1,31 @@
+import { call, put } from 'redux-saga/effects'
+import { commentsLoaded } from '../../actions/comments'
+import { snapshotToArray } from '../../utils/firebase'
+import {
+  addComment, loadComments,
+  getComments, saveComment
+} from '../comments'
+
+describe('comments', () => {
+  describe('addComment', () => {
+    it('saves comment into some kind of datastore', () => {
+      const comment = 'it is a comment'
+      const action = { comment }
+      const gen = addComment(action)
+
+      expect(gen.next().value).toEqual(call(saveComment, comment))
+    })
+  })
+
+  describe('loadComments', () => {
+    it('loads comments from some kind of datastore', () => {
+      const snapshot = 'snap mock'
+      const comments = 'comments mock'
+      const gen = loadComments()
+
+      expect(gen.next().value).toEqual(call(getComments))
+      expect(gen.next(snapshot).value).toEqual(call(snapshotToArray, snapshot))
+      expect(gen.next(comments).value).toEqual(put(commentsLoaded(comments)))
+    })
+  })
+})
--- src/sagas/comments.js
+++ src/sagas/comments.js
@@ -2,19 +2,19 @@ import { takeLatest, put, call } from 'redux-saga/effects'
 import { getDatabase, snapshotToArray } from '../utils/firebase'
 import { LOAD_COMMENTS, ADD_COMMENT, commentsLoaded } from '../actions/comments'
 
-const getComments = () => getDatabase().ref('/comments').once('value')
-const saveComment = (comment) => {
+export const getComments = () => getDatabase().ref('/comments').once('value')
+export const saveComment = (comment) => {
   const newCommentRef = getDatabase().ref('/comments').push()
   newCommentRef.set(comment)
 }
 
-function *loadComments() {
+export function *loadComments() {
   const snapshot = yield call(getComments)
   const comments = yield call(snapshotToArray, snapshot)
   yield put(commentsLoaded(comments))
 }
 
-function *addComment({comment}) {
+export function *addComment({comment}) {
   yield call(saveComment, comment)
 }
 
